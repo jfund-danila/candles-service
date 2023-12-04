0f615692f078a5c2ccd5ebbb6eb4aa59654c3ebf
danila nikitin <danila.n2015@yandex.ru>
2023-12-04 16:57:34 +0300

    initial

---
+++ src/main/java/com/jfund/candlesservice/CandlesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jfund.candlesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandlesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandlesServiceApplication.class, args);
+	}
+
+}
