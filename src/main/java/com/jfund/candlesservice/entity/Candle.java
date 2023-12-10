a9f852b83bd5e62daf0120fd75479555953d1e2e
danila nikitin <danila.n2015@yandex.ru>
2023-12-10 19:13:42 +0300

    delete unused import

--- src/main/java/com/jfund/candlesservice/entity/Candle.java
+++ src/main/java/com/jfund/candlesservice/entity/Candle.java
@@ -1,8 +1,6 @@
 package com.jfund.candlesservice.entity;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.PersistenceCreator;
 import org.springframework.data.mongodb.core.index.Indexed;
